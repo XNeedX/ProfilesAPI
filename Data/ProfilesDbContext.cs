c9ebb1eab1bf314cdbfb233a420217e13582b65c
NeedX <hramko2016@yandex.ru>
2026-04-09 00:46:44 +0300

    feat: added US-8, US-11, US-12 functionality

---
+++ Data/ProfilesDbContext.cs
@@ -0,0 +1,49 @@
+﻿using InnoClinic.Profiles.Api.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Numerics;
+
+public class ProfilesDbContext : DbContext
+{
+    public ProfilesDbContext(DbContextOptions<ProfilesDbContext> options) : base(options) { }
+
+    public DbSet<PatientProfile> Profiles => Set<PatientProfile>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<PatientProfile>(entity =>
+        {
+            entity.ToTable("PatientProfiles");
+
+            entity.HasKey(e => e.Id);
+
+            entity.Property(e => e.FirstName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(e => e.LastName)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(e => e.MiddleName)
+                .HasMaxLength(100)
+                .IsRequired(false);
+
+            entity.Property(e => e.PhoneNumber)
+                .IsRequired()
+                .HasMaxLength(20);
+
+            entity.Property(e => e.DateOfBirth)
+                .IsRequired()
+                .HasColumnType("date");
+
+            entity.Property(e => e.IsEmailVerified)
+                .HasDefaultValue(false);
+
+            entity.Property(e => e.IsLinkedToAccount)
+                .HasDefaultValue(false);
+
+            entity.HasIndex(e => new { e.LastName, e.FirstName, e.MiddleName });
+        });
+
+    }
+}
